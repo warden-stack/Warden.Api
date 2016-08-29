9ca51f5f6d3eb1ef8823941860468a668cf4024e
Piotr Gankiewicz <spetz90@gmail.com>
2016-08-29 13:08:18 +0200

    Endpoint for fetching warden configurations #20

--- src/Warden.Api/Controllers/WardenConfigurationsController.cs
+++ src/Warden.Api/Controllers/WardenConfigurationsController.cs
@@ -2,7 +2,7 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Warden.Api.Infrastructure.Commands;
-using Warden.Api.Infrastructure.Commands.Wardens;
+using Warden.Api.Infrastructure.DTO.WardenConfigurations;
 using Warden.Api.Infrastructure.Services;
 
 namespace Warden.Api.Controllers
@@ -16,12 +16,11 @@ namespace Warden.Api.Controllers
         {
         }
 
-        [HttpPost]
-        public async Task Post([FromBody] SpawnWarden request) =>
-            await For(MapTo<SpawnWarden>(request))
-                .ExecuteAsync(c => CommandDispatcher.DispatchAsync(c))
-                .OnFailure(ex => StatusCode(400))
-                .OnSuccess(c => StatusCode(200))
-                .HandleAsync();
+        [HttpGet]
+        public async Task<WardenConfigurationDto> Get(string id, string token) =>
+            await Task.FromResult(new WardenConfigurationDto
+            {
+                Configuration = "test"
+            });
     }
 }
--- src/Warden.Api/Controllers/WardenSpawnController.cs
+++ src/Warden.Api/Controllers/WardenSpawnController.cs
@@ -3,23 +3,22 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Warden.Api.Infrastructure.Commands;
 using Warden.Api.Infrastructure.Commands.Wardens;
-using Warden.Api.Infrastructure.DTO.Wardens;
 using Warden.Api.Infrastructure.Services;
 
 namespace Warden.Api.Controllers
 {
-    public class WardensController : ControllerBase
+    public class WardenSpawnController : ControllerBase
     {
-        public WardensController(ICommandDispatcher commandDispatcher, 
-            IMapper mapper, 
-            IUserService userService) 
+        public WardenSpawnController(ICommandDispatcher commandDispatcher,
+            IMapper mapper,
+            IUserService userService)
             : base(commandDispatcher, mapper, userService)
         {
         }
 
         [HttpPost]
-        public async Task Post([FromBody] WardenDto request) =>
-            await For(MapTo<CreateWarden>(request))
+        public async Task Post([FromBody] SpawnWarden request) =>
+            await For(MapTo<SpawnWarden>(request))
                 .ExecuteAsync(c => CommandDispatcher.DispatchAsync(c))
                 .OnFailure(ex => StatusCode(400))
                 .OnSuccess(c => StatusCode(200))
