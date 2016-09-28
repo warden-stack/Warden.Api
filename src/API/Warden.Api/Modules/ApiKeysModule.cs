052f19e0c0f14e626b17773d1739eccaeac8adaa
Grzegorz Paśnik <grzesiekmos@gmail.com>
2016-09-28 12:04:09 +0200

    api keys module added #39

---
+++ src/API/Warden.Api/Modules/ApiKeysModule.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Autofac.Core;
+using Nancy.ModelBinding;
+using Warden.Api.Core.Commands;
+using Warden.Api.Core.Storage;
+using Warden.Api.Modules.Base;
+using Warden.Common.Commands.ApiKeys;
+
+namespace Warden.Api.Modules
+{
+    public class ApiKeysModule : AuthenticatedModule
+    {
+        private readonly IApiKeyStorage _apiKeyStorage;
+
+        public ApiKeysModule(ICommandDispatcher commandDispatcher,
+            IApiKeyStorage apiKeyStorage) 
+            : base(commandDispatcher, modulePath: "api-keys")
+        {
+            _apiKeyStorage = apiKeyStorage;
+            Get("/", async args =>
+            {
+                var apiKeys = await _apiKeyStorage.BrowseAsync(CurrentUserId);
+
+                return apiKeys.HasValue ? apiKeys.Value : new List<string>();
+            });
+
+            Post("/", async args =>
+            {
+                var command = this.Bind<RequestNewApiKey>();
+                await CommandDispatcher.DispatchAsync(command);
+            });
+
+            Delete("/", async args =>
+            {
+                var command = this.Bind<DeleteApiKey>();
+                await CommandDispatcher.DispatchAsync(command);
+            });
+        }
+    }
+}
--- src/API/Warden.Api/Modules/Base/AuthenticatedModule.cs
+++ src/API/Warden.Api/Modules/Base/AuthenticatedModule.cs
@@ -1,14 +1,33 @@
 ﻿using Nancy.Security;
 using Warden.Api.Core.Commands;
+using Warden.Common.Extensions;
 
 namespace Warden.Api.Modules.Base
 {
     public class AuthenticatedModule : ModuleBase
     {
+        private string _currentUserId;
+
         public AuthenticatedModule(ICommandDispatcher commandDispatcher, string modulePath = "")
             : base(commandDispatcher, modulePath)
         {
             this.RequiresAuthentication();
+        }
+
+        protected string CurrentUserId
+        {
+            get
+            {
+                if (_currentUserId.Empty())
+                    SetCurrentUserId(Context.CurrentUser?.Identity?.Name?.Replace("auth0|", string.Empty));
+
+                return _currentUserId;
+            }
+        }
+
+        protected void SetCurrentUserId(string id)
+        {
+            _currentUserId = id;
         }
     }
 }
