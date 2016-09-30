04991cc4e854de3eeded9d9f30ad7758ec242fdf
Piotr Gankiewicz <spetz90@gmail.com>
2016-09-30 21:31:57 +0200

    Created synchronizer project #45

---
+++ src/Tools/Warden.Tools.Synchronizer/Modules/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Warden.Tools.Synchronizer.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args => Response.AsJson(new { name = "Warden.Tools.Synchronizer" }));
+        }
+    }
+}
---
+++ src/Tools/Warden.Tools.Synchronizer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Warden.Services.Host;
+using Warden.Tools.Synchronizer.Framework;
+
+namespace Warden.Tools.Synchronizer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebServiceHost
+                .Create<Startup>(port: 10100)
+                .UseAutofac(Bootstrapper.LifetimeScope)
+                .UseRabbitMq()
+                .Build()
+                .Run();
+        }
+    }
+}
